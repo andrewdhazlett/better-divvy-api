a89daaeafafad668907d613c04cb56603cb2868c
andrewdhazlett <andrew@hazlett.me>
2019-10-30 17:53:06 -0500

    generate stations controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StationsController } from './stations/stations.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, StationsController],
   providers: [AppService],
 })
 export class AppModule {}
