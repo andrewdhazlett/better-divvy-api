46d6f9b1adddc925281c2cd752253f0cf791578d
andrewdhazlett <andrew@hazlett.me>
2019-11-05 17:22:35 -0600

    use cluster module to utilize multi-core cpu

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import * as cluster from 'cluster'
+import * as os from 'os'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(`Worker ${process.pid} started`);
 }
-bootstrap();
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  for (const _ of os.cpus()) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  bootstrap();
+}
